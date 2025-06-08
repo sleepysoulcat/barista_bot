d3437b59e510ba889adf136b5bdb460a18e46eb7
Debora Morais <dependentwinter@gmail.com>
2025-06-08 06:40:47 -0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,9 +1,12 @@
 from aiohttp import web
 
 async def ping(request):
-    return web.Response(text="Ainda viva, por favor pague um plano para mimmmmm")
+    return web.Response(text="✅ Estou vivo!")
 
-def iniciar_webserver():
+async def iniciar_webserver():
     app = web.Application()
-    app.router.add_get('/', ping)
-    web.run_app(app, port=3000) 
+    app.router.add_get("/", ping)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, port=3000)
+    await site.start()
