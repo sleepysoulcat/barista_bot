5c4f0eded41712e0ea4ef6737477963f9cf9672d
Debora Morais <dependentwinter@gmail.com>
2025-06-08 07:08:15 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,22 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-import os
+import asyncio
+
 from config import TOKEN, GUILD_ID
 from comandos import registrar_comandos
 from prefixos import registrar_comandos_prefixo
-from keep_alive import iniciar_webserver  
+from keep_alive import iniciar_webserver
 
-# Intents
 intents = discord.Intents.default()
 intents.message_content = True
-
-
 bot = commands.Bot(command_prefix="Eli ", intents=intents)
 tree = bot.tree
 
 @bot.event
 async def on_ready():
     print(f"🤖 Bot online como {bot.user}")
+
     guild = discord.Object(id=GUILD_ID)
 
     tree.clear_commands(guild=guild)
@@ -27,6 +26,10 @@ async def on_ready():
     await tree.sync(guild=guild)
     print("✅ Comandos sincronizados.")
 
-iniciar_webserver()
+async def main():
+    # Inicia o webserver e o bot em paralelo
+    await iniciar_webserver()
+    await bot.start(TOKEN)
 
-bot.run(TOKEN)
+# Executa a função principal
+asyncio.run(main())
