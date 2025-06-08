a7b535312c48de6c873db29c515e8b38e2c3c111
Debora Morais <dependentwinter@gmail.com>
2025-06-08 07:00:52 -0300

    Update comandos.py

--- comandos.py
+++ comandos.py
@@ -1,7 +1,6 @@
 from discord import app_commands, Interaction, Object
 from config import GUILD_ID
 
-# Comando: /falar (só para moderadores)
 @app_commands.command(name="falar", description="O bot repete a mensagem (somente mods podem usar)")
 @app_commands.describe(mensagem="Mensagem para o bot falar")
 async def falar(interaction: Interaction, mensagem: str):
@@ -12,7 +11,6 @@ async def falar(interaction: Interaction, mensagem: str):
     await interaction.response.send_message(mensagem)
 
 
-# Função para registrar os comandos no bot
 def registrar_comandos(tree: app_commands.CommandTree):
     tree.add_command(falar, guild=Object(id=GUILD_ID))
 
